9b96c3ccfda2b6a31b9f563d1f24d5c87212832c
Olga MaciaszekSharma <olga.maciaszek@gmail.com>
2021-06-22 20:23:55 +0200

    Switch to DSL-based helper methods.

--- src/test/resources/contracts/shouldReturnAllCats.groovy
+++ src/test/resources/contracts/shouldReturnAllCats.groovy
@@ -1,19 +1,16 @@
 import org.springframework.cloud.contract.spec.Contract
-import org.springframework.cloud.contract.spec.internal.HttpMethods
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 
 Contract.make {
     description("should return the cat by name")
     request {
         url("/cats/Toby")
-        method(HttpMethods.HttpMethod.GET.methodName)
+        method(GET())
     }
     response {
-        status(HttpStatus.OK.value())
+        status(OK())
         body([name: "Toby"])
         headers {
-            contentType(MediaType.APPLICATION_JSON_VALUE)
+            contentType(applicationJson())
         }
 
     }
