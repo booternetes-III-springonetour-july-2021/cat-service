7e3946379367ae9f1696f8bd9d8d548598d1e0ca
Josh Long <josh@joshlong.com>
2021-06-24 15:18:33 -0700

    service

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -6,3 +6,5 @@ export PROJECT_ID=${GCLOUD_PROJECT:-pgtm-jlong}
 export ROOT_DIR=$(cd $(dirname $0) && pwd)
 
 mvn clean deploy
+
+git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git  --force
