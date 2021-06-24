ee3d18eddbef7d5fca1be35de8130d2e19357665
Josh Long <josh@joshlong.com>
2021-06-24 15:33:59 -0700

    simplified deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,18 +2,6 @@
 set -e
 set -o pipefail
 
-RELEASE_BRANCH=release
-ROOT_DIR=$(cd $(dirname $0) && pwd)
-PROJECT_ID=${GCLOUD_PROJECT:-pgtm-jlong}
+mvn clean deploy && \
+  git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
 
-
-function move_code_to_release() {
-#git checkout $RELEASE_BRANCH
-#$ git push <remote> <local_branch>:<remote_name>
-
-git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git  main:release --force
-}
-
-# todo force these things to be chained one to the other
-#mvn clean deploy
-move_code_to_release
