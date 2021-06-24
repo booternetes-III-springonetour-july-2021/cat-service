0a61d6c4bd9fb284d2210bf452145472159b7f26
Josh Long <josh@joshlong.com>
2021-06-24 15:43:50 -0700

    take3

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,6 +2,7 @@
 set -e
 set -o pipefail
 
-mvn clean deploy && \
-  git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
+#mvn clean deploy && \
+  git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
+#  git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
 
