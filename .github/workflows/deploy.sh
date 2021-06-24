0328b72ab18f267c537928606c337a38fe2fcd46
Josh Long <josh@joshlong.com>
2021-06-24 15:50:46 -0700

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 set -e
 set -o pipefail
-
+echo $GIT_USERNAME
+git config user.name "Booternetes CI Bot"
+git config user.email "<>"
 #mvn clean deploy && \
-  git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
+ git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
 #  git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
 
