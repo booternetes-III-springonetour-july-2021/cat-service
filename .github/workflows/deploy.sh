701440dd34bad0ffd1ce2d7e7883d3e70222d1e8
Josh Long <josh@joshlong.com>
2021-06-24 15:30:34 -0700

    improving the deploy.sh

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,13 +2,18 @@
 set -e
 set -o pipefail
 
-export PROJECT_ID=${GCLOUD_PROJECT:-pgtm-jlong}
-export ROOT_DIR=$(cd $(dirname $0) && pwd)
+RELEASE_BRANCH=release
+ROOT_DIR=$(cd $(dirname $0) && pwd)
+PROJECT_ID=${GCLOUD_PROJECT:-pgtm-jlong}
 
-function move_code_to_release(){
- git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git  --force
+
+function move_code_to_release() {
+#git checkout $RELEASE_BRANCH
+#$ git push <remote> <local_branch>:<remote_name>
+
+git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git  main:release --force
 }
 
-mvn clean deploy && move_code_to_release
-
-
+# todo force these things to be chained one to the other
+#mvn clean deploy
+move_code_to_release
