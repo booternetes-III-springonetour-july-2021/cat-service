886ff2e00d63f9a056e20b4decb2b0367f9efe64
Josh Long <josh@joshlong.com>
2021-06-24 17:11:10 -0700

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -14,14 +14,9 @@ mkdir -p $RC
 git clone $RCURL $RC
 cd $RC
 git rm -rf .
-
 git pull $START  main
-#git merge main release
-#git branch -d release
-#git checkout -b release
-#git pull $START main
-#git push origin release
+
 #
 #git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
-#  git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
-
+##  git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
+#
