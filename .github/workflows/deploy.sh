07dbe98f23ef2a7dbcd42047a13166366b433f5f
Josh Long <josh@joshlong.com>
2021-06-24 17:28:40 -0700

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -11,18 +11,12 @@ RC=$HOME/Desktop/release_clone
 RCURL=https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git
 rm -rf $RC && mkdir -p $RC || echo "couldn't create the clone directory"
 git clone $RCURL $RC
-cd $RC && git rm -rf . && git commit -am au\ revoir && git checkout -b work
-cd $START
-git push  $RC main:release --force
-#cd $RC
-#pwd
-#git rm -rf . && git reset   .
-#git commit -am goodbye
-##git branch -a
-##git pull   --force $START  main
-##git merge -s recursive -X theirs
 
-ls -la $RC
+cd $RC && git rm -rf . && git commit -am au\ revoir && git checkout -b work
+
+cd $START && git push  $RC main:release --force
+
+cd $RC && git checkout release &&  ls -la $RC
 
 #
 #git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
