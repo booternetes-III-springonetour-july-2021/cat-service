46d57501e047faa62b5753db387655efa0a2d4d5
Josh Long <josh@joshlong.com>
2021-06-24 17:18:13 -0700

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -15,7 +15,7 @@ pwd  && exit  0
 git rm -rf .
 git commit -am goodbye
 #git branch -a
-git pull $START  main
+git pull --force $START  main
 
 #
 #git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
