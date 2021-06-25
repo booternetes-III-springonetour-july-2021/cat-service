39c20af78bb41c2816cd46fe9c1e50938d0bba6b
Josh Long <josh@joshlong.com>
2021-06-24 17:12:53 -0700

    up

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -2,15 +2,12 @@
 set -e
 set -o pipefail
 echo $GIT_USERNAME
-#git config user.name "Booternetes CI Bot"
-#git config user.email "<>"
-#mvn clean deploy && \
+
 
 START=$(cd `dirname $0` && pwd )/../..
 RC=$HOME/Desktop/release_clone
 RCURL=https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git
-rm -rf $RC
-mkdir -p $RC
+rm -rf $RC && mkdir -p $RC || echo "couldn't create the clone directory"
 git clone $RCURL $RC
 cd $RC
 git rm -rf .
