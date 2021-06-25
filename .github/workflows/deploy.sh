086f470a28f3af0fdb569a15902cbac8d3797fe8
Josh Long <josh@joshlong.com>
2021-06-24 17:09:36 -0700

    deploy

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -5,6 +5,23 @@ echo $GIT_USERNAME
 #git config user.name "Booternetes CI Bot"
 #git config user.email "<>"
 #mvn clean deploy && \
- git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
+
+START=$(cd `dirname $0` && pwd )/../..
+RC=$HOME/Desktop/release_clone
+RCURL=https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git
+rm -rf $RC
+mkdir -p $RC
+git clone $RCURL $RC
+cd $RC
+git rm -rf .
+
+git pull $START  main
+#git merge main release
+#git branch -d release
+#git checkout -b release
+#git pull $START main
+#git push origin release
+#
+#git push https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
 #  git push git@github.com:booternetes-III-springonetour-july-2021/cat-service-release.git main:release --force
 
